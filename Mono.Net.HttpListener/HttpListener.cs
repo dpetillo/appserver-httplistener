d5bd51d10c47716346a0265e495b4e12fb4b3520
Derek Petillo <derek.petillo@gmail.com>
2016-05-18 13:43:17 -0400

    add dnx45 support for getcontextasync

--- Mono.Net.HttpListener/HttpListener.cs
+++ Mono.Net.HttpListener/HttpListener.cs
@@ -34,7 +34,7 @@
 using System;
 using System.Collections;
 using System.Threading;
-#if NET45 || DNXCORE50
+#if NET45 || DNXCORE50 || DNX45
 using System.Threading.Tasks;
 #endif
 using System.Net;
@@ -290,7 +290,7 @@ namespace Mono.Net {
 			disposed = true;
 		}
 
-#if NET45 || DNXCORE50
+#if NET45 || DNXCORE50 || DNX45
 		public Task<HttpListenerContext> GetContextAsync ()
 		{
 			return Task<HttpListenerContext>.Factory.FromAsync (BeginGetContext, EndGetContext, null);
